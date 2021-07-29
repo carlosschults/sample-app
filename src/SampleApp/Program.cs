b2f1d4a4fa5d9637f022243f62474b5b78062bc1
Carlos Schults <carlos.schults@gmail.com>
2021-07-29 19:04:19 -0300

    Implement functionality

--- src/SampleApp/Program.cs
+++ src/SampleApp/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Octokit;
+using System;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Linq;
 
 namespace SampleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Please type the username for the desired user:");
+            var username = ReadLine();
+
+            var github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
+
+            try
+            {
+                var user = await github.User.Get(username);
+                WriteLine($"The user {user.Name} was succesfully retrieved!");
+                WriteLine($"{user.Name} has {user.PublicRepos} public repositories. Do you want to see the list? (y/n)");
+                var response = ReadLine();
+
+                if (string.Equals(
+                    "Y",
+                    response,
+                    StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var repos = await github.Repository.GetAllForUser(username);
+                    foreach (var repo in repos.OrderBy(x => x.CreatedAt))
+                    {
+                        WriteLine($"{repo.CreatedAt:yyyy-MM-dd} | {repo.Name}");
+                    }
+                }
+            }
+            catch (NotFoundException)
+            {
+                WriteLine($"{username} isn't a valid GitHub user!");
+            }
+
+            WriteLine("Press any key to exit.");
+            ReadLine();
         }
     }
 }
